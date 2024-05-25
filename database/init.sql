c1f85a2ed697a8205e05a6c979b5a787ce008507
Bruno Bucci <bruno@brunobucci.com>
2024-05-25 16:52:13 -0300

    Correção de encoding

--- database/init.sql
+++ database/init.sql
@@ -1,19 +1,28 @@
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-START TRANSACTION;
-SET time_zone = "+00:00";
+-- MySQL dump 10.13  Distrib 8.0.36, for macos14 (arm64)
+--
+-- Host: 127.0.0.1    Database: soat7grupo5
+-- ------------------------------------------------------
+-- Server version	8.4.0
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 --
--- Banco de dados: `soat7grupo5`
---
-
--- --------------------------------------------------------
-
---
--- Estrutura da tabela `cliente`
+-- Table structure for table `cliente`
 --
 
 DROP TABLE IF EXISTS `cliente`;
-CREATE TABLE IF NOT EXISTS `cliente` (
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `cliente` (
   `id_cliente` int NOT NULL AUTO_INCREMENT,
   `Nome` varchar(255) CHARACTER SET ucs2 COLLATE ucs2_general_ci DEFAULT NULL,
   `Email` varchar(255) DEFAULT NULL,
@@ -21,40 +30,48 @@ CREATE TABLE IF NOT EXISTS `cliente` (
   `id_campanha` int NOT NULL DEFAULT '0',
   PRIMARY KEY (`id_cliente`)
 ) ENGINE=MyISAM AUTO_INCREMENT=2 DEFAULT CHARSET=ucs2;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Extraindo dados da tabela `cliente`
+-- Dumping data for table `cliente`
 --
 
-INSERT INTO `cliente` (`id_cliente`, `Nome`, `Email`, `cpf`, `id_campanha`) VALUES
-(1, 'Marcelo Nome Completo', 'marcelo-email@email.com.br', '34133413831', 2);
-
--- --------------------------------------------------------
+LOCK TABLES `cliente` WRITE;
+/*!40000 ALTER TABLE `cliente` DISABLE KEYS */;
+INSERT INTO `cliente` VALUES (1,'Marcelo Nome Completo','marcelo-email@email.com.br','34133413831',2);
+/*!40000 ALTER TABLE `cliente` ENABLE KEYS */;
+UNLOCK TABLES;
 
 --
--- Estrutura da tabela `cliente_seq`
+-- Table structure for table `cliente_seq`
 --
 
 DROP TABLE IF EXISTS `cliente_seq`;
-CREATE TABLE IF NOT EXISTS `cliente_seq` (
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `cliente_seq` (
   `next_val` bigint DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=ucs2;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Extraindo dados da tabela `cliente_seq`
+-- Dumping data for table `cliente_seq`
 --
 
-INSERT INTO `cliente_seq` (`next_val`) VALUES
-(51);
-
--- --------------------------------------------------------
+LOCK TABLES `cliente_seq` WRITE;
+/*!40000 ALTER TABLE `cliente_seq` DISABLE KEYS */;
+INSERT INTO `cliente_seq` VALUES (51);
+/*!40000 ALTER TABLE `cliente_seq` ENABLE KEYS */;
+UNLOCK TABLES;
 
 --
--- Estrutura da tabela `pedido`
+-- Table structure for table `pedido`
 --
 
 DROP TABLE IF EXISTS `pedido`;
-CREATE TABLE IF NOT EXISTS `pedido` (
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `pedido` (
   `id_pedido` int NOT NULL,
   `id_cliente` int DEFAULT NULL,
   `id_status` int DEFAULT NULL,
@@ -64,15 +81,25 @@ CREATE TABLE IF NOT EXISTS `pedido` (
   `data_criacao` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   PRIMARY KEY (`id_pedido`)
 ) ENGINE=MyISAM DEFAULT CHARSET=ucs2;
-
--- --------------------------------------------------------
+/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Estrutura da tabela `pedido_produto`
+-- Dumping data for table `pedido`
+--
+
+LOCK TABLES `pedido` WRITE;
+/*!40000 ALTER TABLE `pedido` DISABLE KEYS */;
+/*!40000 ALTER TABLE `pedido` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `pedido_produto`
 --
 
 DROP TABLE IF EXISTS `pedido_produto`;
-CREATE TABLE IF NOT EXISTS `pedido_produto` (
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `pedido_produto` (
   `id_pedido_produto` int NOT NULL AUTO_INCREMENT,
   `id_pedido` int NOT NULL,
   `id_produto` int NOT NULL,
@@ -82,64 +109,92 @@ CREATE TABLE IF NOT EXISTS `pedido_produto` (
   PRIMARY KEY (`id_pedido_produto`),
   KEY `FK1sojgctn8vt21b1tbuaou288m` (`id_pedido`)
 ) ENGINE=MyISAM AUTO_INCREMENT=54 DEFAULT CHARSET=ucs2;
-
--- --------------------------------------------------------
+/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Estrutura da tabela `pedido_produto_seq`
+-- Dumping data for table `pedido_produto`
+--
+
+LOCK TABLES `pedido_produto` WRITE;
+/*!40000 ALTER TABLE `pedido_produto` DISABLE KEYS */;
+/*!40000 ALTER TABLE `pedido_produto` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `pedido_produto_seq`
 --
 
 DROP TABLE IF EXISTS `pedido_produto_seq`;
-CREATE TABLE IF NOT EXISTS `pedido_produto_seq` (
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `pedido_produto_seq` (
   `next_val` bigint DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=ucs2;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Extraindo dados da tabela `pedido_produto_seq`
+-- Dumping data for table `pedido_produto_seq`
 --
 
-INSERT INTO `pedido_produto_seq` (`next_val`) VALUES
-(151);
-
--- --------------------------------------------------------
+LOCK TABLES `pedido_produto_seq` WRITE;
+/*!40000 ALTER TABLE `pedido_produto_seq` DISABLE KEYS */;
+INSERT INTO `pedido_produto_seq` VALUES (151);
+/*!40000 ALTER TABLE `pedido_produto_seq` ENABLE KEYS */;
+UNLOCK TABLES;
 
 --
--- Estrutura da tabela `pedido_seq`
+-- Table structure for table `pedido_seq`
 --
 
 DROP TABLE IF EXISTS `pedido_seq`;
-CREATE TABLE IF NOT EXISTS `pedido_seq` (
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `pedido_seq` (
   `next_val` bigint DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=ucs2;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Extraindo dados da tabela `pedido_seq`
+-- Dumping data for table `pedido_seq`
 --
 
-INSERT INTO `pedido_seq` (`next_val`) VALUES
-(451);
-
--- --------------------------------------------------------
+LOCK TABLES `pedido_seq` WRITE;
+/*!40000 ALTER TABLE `pedido_seq` DISABLE KEYS */;
+INSERT INTO `pedido_seq` VALUES (451);
+/*!40000 ALTER TABLE `pedido_seq` ENABLE KEYS */;
+UNLOCK TABLES;
 
 --
--- Estrutura da tabela `pedido_status`
+-- Table structure for table `pedido_status`
 --
 
 DROP TABLE IF EXISTS `pedido_status`;
-CREATE TABLE IF NOT EXISTS `pedido_status` (
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `pedido_status` (
   `ID_pedido_status` int NOT NULL,
   `nome` varchar(255) NOT NULL,
   PRIMARY KEY (`ID_pedido_status`)
 ) ENGINE=MyISAM DEFAULT CHARSET=ucs2;
-
--- --------------------------------------------------------
+/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Estrutura da tabela `produto`
+-- Dumping data for table `pedido_status`
+--
+
+LOCK TABLES `pedido_status` WRITE;
+/*!40000 ALTER TABLE `pedido_status` DISABLE KEYS */;
+/*!40000 ALTER TABLE `pedido_status` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `produto`
 --
 
 DROP TABLE IF EXISTS `produto`;
-CREATE TABLE IF NOT EXISTS `produto` (
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `produto` (
   `ID_Produto` int NOT NULL AUTO_INCREMENT,
   `Nome` varchar(255) NOT NULL,
   `Descricao` varchar(255) DEFAULT NULL,
@@ -149,57 +204,71 @@ CREATE TABLE IF NOT EXISTS `produto` (
   `fl_ativo` int NOT NULL DEFAULT '1',
   PRIMARY KEY (`ID_Produto`)
 ) ENGINE=MyISAM AUTO_INCREMENT=7 DEFAULT CHARSET=ucs2;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Extraindo dados da tabela `produto`
+-- Dumping data for table `produto`
 --
 
-INSERT INTO `produto` (`ID_Produto`, `Nome`, `Descricao`, `preco`, `duracao_preparo`, `id_tipo`, `fl_ativo`) VALUES
-(1, 'Misto quente', 'Pão francês com presunto e queijo na chapa.', 10, 5, 1, 1),
-(2, 'Queijo quente', 'Pão francês com queijo e tomate na chapa.', 8, 3, 1, 1),
-(3, 'Suco de laranja', 'Suco de laranja 500ml.', 12, 7, 3, 1),
-(4, 'Coca-cola', 'Refrigerante Coca-cola lata 350ml.', 8, 0, 3, 1),
-(5, 'Sorvete', 'Picolé Tablito.', 15, 0, 4, 1),
-(6, 'Cheese Burger', 'Pão de hambúrguer, carne e queijo.', 11, 7, 1, 1);
-
--- --------------------------------------------------------
+LOCK TABLES `produto` WRITE;
+/*!40000 ALTER TABLE `produto` DISABLE KEYS */;
+INSERT INTO `produto` VALUES (1,'Misto quente','Pão francês com presunto e queijo na chapa.',10,5,1,1),(2,'Queijo quente','Pão francês com queijo e tomate na chapa.',8,3,1,1),(3,'Suco de laranja','Suco de laranja 500ml.',12,7,3,1),(4,'Coca-cola','Refrigerante Coca-cola lata 350ml.',8,0,3,1),(5,'Sorvete','Picolé Tablito.',15,0,4,1),(6,'Cheese Burger','Pão de hambúrguer, carne e queijo.',11,7,1,1);
+/*!40000 ALTER TABLE `produto` ENABLE KEYS */;
+UNLOCK TABLES;
 
 --
--- Estrutura da tabela `produto_seq`
+-- Table structure for table `produto_seq`
 --
 
 DROP TABLE IF EXISTS `produto_seq`;
-CREATE TABLE IF NOT EXISTS `produto_seq` (
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `produto_seq` (
   `next_val` bigint DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=ucs2;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Extraindo dados da tabela `produto_seq`
+-- Dumping data for table `produto_seq`
 --
 
-INSERT INTO `produto_seq` (`next_val`) VALUES
-(101);
-
--- --------------------------------------------------------
+LOCK TABLES `produto_seq` WRITE;
+/*!40000 ALTER TABLE `produto_seq` DISABLE KEYS */;
+INSERT INTO `produto_seq` VALUES (101);
+/*!40000 ALTER TABLE `produto_seq` ENABLE KEYS */;
+UNLOCK TABLES;
 
 --
--- Estrutura da tabela `produto_tipo`
+-- Table structure for table `produto_tipo`
 --
 
 DROP TABLE IF EXISTS `produto_tipo`;
-CREATE TABLE IF NOT EXISTS `produto_tipo` (
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `produto_tipo` (
   `ID_produto_tipo` int NOT NULL,
   `nome` varchar(255) NOT NULL,
   PRIMARY KEY (`ID_produto_tipo`)
 ) ENGINE=MyISAM DEFAULT CHARSET=ucs2;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Extraindo dados da tabela `produto_tipo`
+-- Dumping data for table `produto_tipo`
 --
 
-INSERT INTO `produto_tipo` (`ID_produto_tipo`, `nome`) VALUES
-(1, 'Lanche'),
-(2, 'Acompanhamento'),
-(3, 'Bebida'),
-(4, 'Sobremesa');
-COMMIT;
+LOCK TABLES `produto_tipo` WRITE;
+/*!40000 ALTER TABLE `produto_tipo` DISABLE KEYS */;
+INSERT INTO `produto_tipo` VALUES (1,'Lanche'),(2,'Acompanhamento'),(3,'Bebida'),(4,'Sobremesa');
+/*!40000 ALTER TABLE `produto_tipo` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2024-05-25 16:47:04
